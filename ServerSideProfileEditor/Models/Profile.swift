e1773832cdad0bcf4bd695414102a0de174816a1
marquavious <marquavious@users.noreply.github.com>
2024-10-20 20:40:23 -0400

    Added Profile Model

---
+++ ServerSideProfileEditor/Models/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  ServerSideProfileEditor
+//
+//  Created by Marquavious Draggon on 10/20/24.
+//
+
+import Foundation
+
+struct Profile: Identifiable {
+  let id: String
+  var userInfo: User
+//  var sections: [SectionData]
+}
+
+extension Profile: Hashable, Equatable {
+  public func hash(into hasher: inout Hasher) {
+    hasher.combine(id)
+  }
+
+  static func == (lhs: Profile, rhs: Profile) -> Bool {
+    lhs.id == rhs.id &&
+    lhs.userInfo == rhs.userInfo &&
+    lhs.sections == rhs.sections
+  }
+}
