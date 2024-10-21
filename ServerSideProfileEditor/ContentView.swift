8dfe530537084f267e713df57ab62d97a8181427
marquavious <marquavious@users.noreply.github.com>
2024-10-20 20:33:08 -0400

    Initial Commit

---
+++ ServerSideProfileEditor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ServerSideProfileEditor
+//
+//  Created by Marquavious Draggon on 10/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ServerSideProfileEditor/ServerSideProfileEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServerSideProfileEditorApp.swift
+//  ServerSideProfileEditor
+//
+//  Created by Marquavious Draggon on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct ServerSideProfileEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
